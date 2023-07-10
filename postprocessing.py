c29ee9d5334f93f2cd1c685838144fe957bca803
Nate <natelharrison@gmail.com>
2023-07-10 00:01:00 -0700

    Hehe

---
+++ postprocessing.py
@@ -0,0 +1,43 @@
+import itertools
+import logging
+import os
+import re
+import time
+import argparse
+
+import numpy as np
+
+from pathlib import Path
+from shutil import rmtree
+from tifffile import imread, imwrite
+from tqdm.contrib import itertools
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', type=str, default='')
+args = parser.parse_args()
+
+
+def postprocessing(image_files: list):
+    
+    return
+
+def main():
+
+    dir = Path(args.dir)
+    image_files = list(dir.glob('*.tif'))
+
+    print(f"Images found:{len(image_files)}")
+
+    postprocessing(image_files)
+
+main()
+
+
+
+
+
+
+
+
+
