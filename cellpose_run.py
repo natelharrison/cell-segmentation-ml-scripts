f7f7b0af81147fb650b84bd10312e98c03442113
Nate <natelharrison@gmail.com>
2023-07-17 00:24:05 -0700

    cellpose_run.py added args and mask prediction saving

--- cellpose_run.py
+++ cellpose_run.py
@@ -1,19 +1,36 @@
+import os
+import argparse
+from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
-from cellpose import models
+from cellpose import models, io
 from cellpose.io import imread
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', type=str, default='')
+parser.add_argument('--image_dir', type=str, default='')
+parser.add_argument('--model', type=str, default='cyto2')
+args = parser.parse_args()
+
 # model_type='cyto' or 'nuclei' or 'cyto2'
-model = models.Cellpose(model_type='cyto')
+
+model_arg = args.model
+model = models.Cellpose(gpu=True, model_type=model_arg)
 
 # list of files
 # PUT PATH TO YOUR FILES HERE!
-files = ['/media/carsen/DATA1/TIFFS/onechan.tif']
+file_path = Path(args.image_path)
+file_name = file_path.stem
+file = file_path.as_posix()
 
-imgs = [imread(f) for f in files]
-nimg = len(imgs)
+save_dir = file_path.parent / "predictions"
+os.mkdir(save_dir)
+save_dir = save_dir.as_posix()
 
-# define CHANNELS to run segementation on
+image = imread(file)
+
+# define CHANNELS to run segmentation on
 # grayscale=0, R=1, G=2, B=3
 # channels = [cytoplasm, nucleus]
 # if NUCLEUS channel does not exist, set the second channel to 0
@@ -27,4 +44,16 @@ channels = [[0,0]]
 # you can set the average cell `diameter` in pixels yourself (recommended)
 # diameter can be a list or a single number for all images
 
-masks, flows, styles, diams = model.eval(imgs, diameter=None, channels=channels)
+masks, flows, styles, diams = model.eval(image,
+                                         do_3D=True,
+                                         progress=True,
+                                         min_size=1000,
+                                         channels=channels)
+io.save_masks(images=image,
+              masks=masks,
+              flows=flows,
+              file_names=file_name,
+              png=False,
+              tif=True,
+              channels=channels,
+              savedir=save_dir)
