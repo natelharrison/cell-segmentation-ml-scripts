2f278fc459e7591378f12e4472c8f36753ce4d57
Nate <natelharrison@gmail.com>
2023-07-16 23:22:36 -0700

    Added minimum cell sizes script

---
+++ cell_size.py
@@ -0,0 +1,25 @@
+import argparse
+
+import numpy as np
+
+from tifffile import imread
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--image_path', type=str)
+args = parser.parse_args()
+
+
+def main():
+    file_path = Path(args.image_path)
+
+    image = imread(file_path.as_posix())
+
+    _, counts = np.unique(image, return_counts=True)
+
+    image = np.sort(image)
+
+    for i in range(10):
+        print(image[i], "\n")
+
+main()
