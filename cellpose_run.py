fa0603d561ab444b14f0d52e7712aab53339ec0a
Nate <natelharrison@gmail.com>
2023-07-19 16:21:44 -0700

    preprocessing.py: Changed code formatting

--- cellpose_run.py
+++ cellpose_run.py
@@ -16,37 +16,79 @@ logging.basicConfig(level=logging.INFO)
 
 
 # model_type='cyto' or 'nuclei' or 'cyto2'
-def load_cellpose_modelpath(model_path: Path,
-                            gpu: bool = True) -> models.CellposeModel:
+def load_model(
+        model_path: Path,
+        gpu: bool = True
+) -> models.CellposeModel:
+    """
+    Load a Cellpose model from a given path.
 
+    Parameters:
+    model_path (Path): Path to the pretrained model.
+    gpu (bool): If True, use GPU for model prediction. Default is True.
+
+    Returns:
+    models.CellposeModel: The loaded Cellpose model.
+    """
     # load cellpose model
     print('Loading Cellpose Models from folder ...')
-    model = models.CellposeModel(gpu=gpu, pretrained_model=model_path.as_posix())
+    model = models.CellposeModel(
+        gpu=gpu,
+        pretrained_model=model_path.as_posix()
+    )
     print(f"Loaded {model_path.stem}")
 
     return model
 
-def model_predictions(model, image, file_name, channels, save_dir, **kwargs):
-    masks, flows, styles = model.eval(image,
-                                      progress=True,
-                                      channels=channels,
-                                      **kwargs)
-    io.save_masks(images=image,
-                  masks=masks,
-                  flows=flows,
-                  file_names=file_name,
-                  png=False,
-                  tif=True,
-                  channels=channels,
-                  savedir=save_dir)
+
+def model_predictions(
+        model,
+        image,
+        file_name,
+        channels,
+        save_dir,
+        **kwargs
+):
+    """
+    Run a Cellpose model on an image and save the results.
+
+    Parameters:
+    model (models.CellposeModel): The Cellpose model to use for prediction.
+    image (np.ndarray): The image to run the model on.
+    file_name (str): The name of the file to save the results to.
+    channels (list): List of channels to run segmentation on.
+    save_dir (str): The directory where the results will be saved.
+    **kwargs: Additional parameters to pass to the model's eval method.
+
+    Returns:
+    None
+    """
+    masks, flows, styles = model.eval(
+        image,
+        progress=True,
+        channels=channels,
+        **kwargs
+    )
+    io.save_masks(
+        images=image,
+        masks=masks,
+        flows=flows,
+        file_names=file_name,
+        png=False,
+        tif=True,
+        channels=channels,
+        savedir=save_dir
+    )
 
 
 def main():
+    """
+    Main function to run the script. It loads the model, reads the image, and runs the model prediction.
+    The results are saved in the specified directory.
+    """
     model_path = Path(args.model)
-    model = load_cellpose_modelpath(model_path)
+    model = load_model(model_path)
 
-    # list of files
-    # PUT PATH TO YOUR FILES HERE!
     file_path = Path(args.image_path)
     file_name = file_path.stem
     file = file_path.as_posix()
@@ -57,18 +99,13 @@ def main():
 
     image = imread(file)
 
-    # define CHANNELS to run segmentation on
-    # grayscale=0, R=1, G=2, B=3
-    # channels = [cytoplasm, nucleus]
-    # if NUCLEUS channel does not exist, set the second channel to 0
-    channels = [[0,0]]
-    # IF ALL YOUR IMAGES ARE THE SAME TYPE, you can give a list with 2 elements
-    # channels = [0,0] # IF YOU HAVE GRAYSCALE
-    # channels = [2,3] # IF YOU HAVE G=cytoplasm and B=nucleus
-    # channels = [2,1] # IF YOU HAVE G=cytoplasm and R=nucleus
+    channels = [[0,0]]  # define channels to run segmentation on
 
-    # if diameter is set to None, the size of the cells is estimated on a per-image basis
-    # you can set the average cell `diameter` in pixels yourself (recommended)
-    # diameter can be a list or a single number for all images
+    model_predictions(
+        model,
+        image,
+        file_name,
+        channels,
+        save_dir
+    )
 
-    model_predictions(model, image, file_name, channels, save_dir)
