2bddcc04729c4c43c7bd4b1919e0c2beb588f7ec
Nate <natelharrison@gmail.com>
2023-07-19 19:35:18 -0700

    batch_script_generator.py: created

---
+++ batch_script_generator.py
@@ -0,0 +1,59 @@
+import argparse
+import subprocess
+import json
+
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--image', type=str, default='')
+parser.add_argument('--model', type=str, default='cyto2')
+args = parser.parse_args()
+
+# Define your list of kwargs here
+kwargs_list = [
+    {"diameter": 60, "min_size": 1000},
+    # Add more kwargs dictionaries here as needed
+]
+
+
+dir = Path.cwd()
+user_dir = dir.parent
+image_path = args.image
+model_path = args.model
+
+for kwargs in kwargs_list:
+    # Convert kwargs dictionary to a string
+    kwargs_str = json.dumps(kwargs)
+
+    # Create the batch script
+    batch_name = f'{kwargs_str}_{image_path.stem}.log'
+    log_output = dir/'logs'/batch_name
+
+
+    batch_script = f"""
+    #!/bin/sh
+    #SBATCH --qos=abc_normal
+    #SBATCH --gres=gpu:1
+    #SBATCH --partition=abc
+    #SBATCH --account=co_abc
+    #SBATCH --nodes=1
+    #SBATCH --time=8:00:00
+    #SBATCH --ntasks=5
+    #SBATCH --mem=125G
+    #SBATCH --output={log_output}
+    #SBATCH --export=ALL
+
+    ### Run your command
+    . {user_dir}/anaconda3/etc/profile.d/conda.sh
+    conda activate cellpose
+
+    python cellpose_run.py --image_path {image_path} --add_model {model_path} --kwargs '{kwargs_str}' """
+
+    # Save the batch script to a file
+    with open(f'{batch_name}.sh', "w") as file:
+        file.write(batch_script)
+
+    # Run the batch script
+    subprocess.run(["sbatch", f"{batch_name}.sh"])
+
+
