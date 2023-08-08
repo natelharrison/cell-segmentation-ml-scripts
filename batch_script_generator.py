638306cf3be697fe289c4890f5980411ee2c05ae
Thayer <thayeralshaabi@gmail.com>
2023-08-07 17:38:50 -0700

    batch_script_generator.py: now loops through model_list

--- batch_script_generator.py
+++ batch_script_generator.py
@@ -57,38 +57,39 @@ if args.model:
 else:
     model_list = [Path(model) for model in model_list]
 
-for i, kwargs in enumerate(kwargs_list):
-    # Convert kwargs dictionary to a string
-    kwargs_str = json.dumps(kwargs)
+for model in model_list:
+    for i, kwargs in enumerate(kwargs_list):
+        # Convert kwargs dictionary to a string
+        kwargs_str = json.dumps(kwargs)
 
-    # Create the batch script
-    batch_name = f'batch_{i}_{image_path.stem}'
-    log_output = log_dir / f'{batch_name}.log'
+        # Create the batch script
+        batch_name = f'batch_{i}_{image_path.stem}'
+        log_output = log_dir / f'{batch_name}.log'
 
-    model_path = model_list[i % len(model_list)]
+        model_path = model
 
-    # Rest of your script...
-    batch_script = f"""#!/bin/sh
-#SBATCH --qos=abc_normal
-#SBATCH --gres=gpu:1
-#SBATCH --partition=abc
-#SBATCH --account=co_abc
-#SBATCH --nodes=1
-#SBATCH --time=8:00:00
-#SBATCH --ntasks=5
-#SBATCH --mem=125G
-#SBATCH --output={log_output}
-#SBATCH --export=ALL
+        # Rest of your script...
+        batch_script = f"""#!/bin/sh
+    #SBATCH --qos=abc_normal
+    #SBATCH --gres=gpu:1
+    #SBATCH --partition=abc
+    #SBATCH --account=co_abc
+    #SBATCH --nodes=1
+    #SBATCH --time=8:00:00
+    #SBATCH --ntasks=5
+    #SBATCH --mem=125G
+    #SBATCH --output={log_output}
+    #SBATCH --export=ALL
+    
+    ### Run your command
+    . {user_dir}/anaconda3/etc/profile.d/conda.sh
+    conda activate cellpose
+    
+    python cellpose_run.py --image_path {image_path} --model {model_path} --save_name {batch_name} --kwargs '{kwargs_str}' """
 
-### Run your command
-. {user_dir}/anaconda3/etc/profile.d/conda.sh
-conda activate cellpose
+        # Save the batch script to a file
+        with open(save_dir / f'{batch_name}.sh', "w") as file:
+            file.write(batch_script)
 
-python cellpose_run.py --image_path {image_path} --model {model_path} --save_name {batch_name} --kwargs '{kwargs_str}' """
-
-    # Save the batch script to a file
-    with open(save_dir / f'{batch_name}.sh', "w") as file:
-        file.write(batch_script)
-
-    # Run the batch script
-    subprocess.run(["sbatch", (save_dir / f"{batch_name}.sh").as_posix()])
+        # Run the batch script
+        subprocess.run(["sbatch", (save_dir / f"{batch_name}.sh").as_posix()])
