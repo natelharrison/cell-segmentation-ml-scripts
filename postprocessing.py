022b97e3c19db48e8e7dc294b7b2facc21f83e0d
Thayer <thayeralshaabi@gmail.com>
2023-08-07 18:35:12 -0700

    postprocessing.py: created skeleton code

--- postprocessing.py
+++ postprocessing.py
@@ -17,7 +17,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str, default='')
 args = parser.parse_args()
 
-
+def create_save_dir(dir_path: Path):
+    ...
+def filter_labels(image: np.ndarray):
+    ...
+def tile_stitching(tiles: list):
+    ...
 def postprocessing(image_files: list):
     
     return
@@ -26,7 +31,6 @@ def main():
 
     dir = Path(args.dir)
     image_files = list(dir.glob('*.tif'))
-
     print(f"Images found:{len(image_files)}")
 
     postprocessing(image_files)
