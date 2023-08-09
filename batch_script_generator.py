5a03011bf0bf24de6f5273b6a75168b678cfe0ff
Nate <natelharrison@gmail.com>
2023-08-08 17:46:14 -0700

    batch_script_generator.py: created main function and added some code

--- batch_script_generator.py
+++ batch_script_generator.py
@@ -11,9 +11,8 @@ now = datetime.now()
 date_string = now.strftime("%Y-%m-%d_%H-%M-%S")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--image', type=str, default='')
-parser.add_argument('--model', type=str, default='')
-parser.add_argument('--name', type=str, default=date_string)
+parser.add_argument('--image_path', type=str, default=None)
+parser.add_argument('--batch_name', type=str, default=date_string)
 args = parser.parse_args()
 
 kwargs_list = [
@@ -54,7 +53,7 @@ batch_script = """#!/bin/sh
 #SBATCH --time=8:00:00
 #SBATCH --ntasks=5
 #SBATCH --mem=125G
-#SBATCH --output={log_output}
+#SBATCH --output={log_path}
 #SBATCH --export=ALL
 
 ### Run your command
@@ -63,8 +62,38 @@ conda activate cellpose
 
 python cellpose_run.py --image_path {image_path} --model {model_path} --save_name {batch_name} --kwargs '{kwargs_str}' """
 
-def main():
-    ...
+def make_dir(dir_path: Path):
+    if dir_path.exists():
+        os.rmdir(dir_path)
+    os.mkdir(dir_path)
 
-if __name__ == '_main__':
+
+def main():
+    image_path = Path(args.image_path)
+
+    kwargs_str_list = [json.dumps(kwargs) for kwargs in kwargs_list]
+    model_path_list = [Path(model) for model in model_list]
+
+    user_dir = Path.home()
+    batch_name = args.name
+    script_batch_dir = user_dir / 'cellpose_run' / batch_name
+    make_dir(script_batch_dir)
+
+    for model_path in model_path_list:
+        for i, kwargs_str in enumerate(kwargs_str_list):
+            log_path = script_batch_dir / model_path.stem
+            make_dir(log_path)
+
+            batch_script.format(
+                log_path = log_path,
+                user_dir = user_dir,
+                image_path = image_path,
+                model_path = model_path,
+                batch_name = batch_name,
+                kwargs_str = kwargs_str
+            )
+
+            ...
+
+if __name__ == '__main__':
     main()
