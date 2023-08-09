b7fe935052395e2771b0cad57f1f662a4ba0dee4
Nate <natelharrison@gmail.com>
2023-08-08 20:02:15 -0700

    cellpose_run.py: changed prediction save directory structure

--- cellpose_run.py
+++ cellpose_run.py
@@ -5,15 +5,18 @@ from pathlib import Path
 import logging
 from cellpose import models, io
 from cellpose.io import imread
+from datetime import datetime
 
+now = datetime.now()
+date_string = now.strftime("%Y-%m-%d_%H-%M-%S")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str, default='')
 parser.add_argument('--image_path', type=str, default='')
 parser.add_argument('--model', type=str, default='cyto2')
 parser.add_argument('--kwargs', type=str, default='{}')
-parser.add_argument('--batch_name', type=str, default='')
-parser.add_argument('--save_name', type=str, default=None)
+parser.add_argument('--save_name', type=str, default=date_string)
+parser.add_argument('--batch_name', type=str, default=None)
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.INFO)
@@ -29,27 +32,23 @@ def load_model(
 
 
 def main():
-
     model_path = Path(args.model)
     model = load_model(model_path)
 
-    file_path = Path(args.image_path)
-    file_name = file_path.stem
-    file = file_path.as_posix()
+    image_path = Path(args.image_path)
+    image_name = image_path.stem
 
-
+    save_name = args.save_name
     batch_name = args.batch_name
-    if not batch_name:
-        batch_name = model_path.stem
 
-    save_dir = file_path.parent / f"{batch_name}_predictions"
-    if args.save_name:
-        save_dir = save_dir / args.save_name
+    save_dir = image_path.parent / save_name
+    if batch_name is not None:
+        save_dir = save_dir / model_path.stem
+        image_name = f"{batch_name}_{image_name}"
     os.makedirs(save_dir, exist_ok=True)
-    save_dir = save_dir.as_posix()
 
     channels = [[0,0]]
-    image = imread(file)
+    image = imread(image_path.as_posix())
 
     try:
         kwargs = json.loads(args.kwargs)
@@ -72,7 +71,7 @@ def main():
         images=image,
         masks=masks,
         flows=flows,
-        file_names=file_name,
+        file_names=image_name,
         png=False,
         tif=True,
         save_txt=False,
