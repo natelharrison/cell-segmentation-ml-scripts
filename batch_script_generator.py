1787c42e4acd82affda99efa1fcbb0aef0d1b04c
Nate <natelharrison@gmail.com>
2023-08-08 17:04:12 -0700

    batch_script_generator.py: rewriting, created main function and make batch_script string variable

--- batch_script_generator.py
+++ batch_script_generator.py
@@ -42,34 +42,7 @@ model_list = [
     "/clusterfs/fiona/segmentation_curation/cellpose_training/trained_models/training_trial_2/cellpose_residual_on_style_on_concatenation_off_cropping_output_2023_07_01_18_46_14.616214"
 ]
 
-dir = Path.cwd()
-user_dir = dir.parent
-save_dir = user_dir / 'cellpose_run' / args.name
-log_dir = save_dir
-
-if save_dir.exists():
-    rmtree(save_dir)
-os.mkdir(save_dir)
-
-image_path = Path(args.image)
-if args.model:
-    model_list = [Path(args.model)]
-else:
-    model_list = [Path(model) for model in model_list]
-
-for model in model_list:
-    for i, kwargs in enumerate(kwargs_list):
-        # Convert kwargs dictionary to a string
-        kwargs_str = json.dumps(kwargs)
-
-        # Create the batch script
-        batch_name = f'batch_{i}_{image_path.stem}'
-        log_output = log_dir / f'{batch_name}.log'
-
-        model_path = model
-
-        # Rest of your script...
-        batch_script = f"""#!/bin/shdfadfasdfadfas
+batch_script = """#!/bin/shdfadfasdfadfas
 #SBATCH --qos=abc_normald
 #SBATCH --gres=gpu:1
 #SBATCH --partition=abc
@@ -80,16 +53,15 @@ for model in model_list:
 #SBATCH --mem=125G
 #SBATCH --output={log_output}
 #SBATCH --export=ALL
-    
+
 ### Run your command
 . {user_dir}/anaconda3/etc/profile.d/conda.sh
 conda activate cellpose
-    
+
 python cellpose_run.py --image_path {image_path} --model {model_path} --save_name {batch_name} --kwargs '{kwargs_str}' """
 
-        # Save the batch script to a file
-        with open(save_dir / f'{batch_name}.sh', "w") as file:
-            file.write(batch_script)
+def main():
+    ...
 
-        # Run the batch script
-        subprocess.run(["sbatch", (save_dir / f"{batch_name}.sh").as_posix()])
+if __name__ == '_main__':
+    main()
