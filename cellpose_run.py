091f4941061dcbe2ffb71279f2c64fc823a82310
Nate <natelharrison@gmail.com>
2023-08-09 18:58:12 -0700

    cellpose_run.py: Some progress on tiling

--- cellpose_run.py
+++ cellpose_run.py
@@ -4,6 +4,7 @@ import dask
 import logging
 import argparse
 
+import dask.array as da
 
 from pathlib import Path
 from tifffile import imwrite
@@ -18,7 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str, default='')
 parser.add_argument('--image_path', type=str, default='')
 parser.add_argument('--model', type=str, default=None)
-parser.add_argument('--tile_size', type=int, nargs='+', default=None)
+parser.add_argument('--chunks', type=int, nargs='+', default=None)
 parser.add_argument('--kwargs', type=str,
     default='{"diameter": 30, "do_3D": true, "resample": true, "min_size": 2000, "augment": true}')
 parser.add_argument('--save_name', type=str, default=date_string)
@@ -40,8 +41,17 @@ def run_predictions(model, image, channels, **kwargs):
     return model.eval(image, progress=True, channels=channels, **kwargs)
 
 
-def tile_image():
-    ...
+def tile_image(image_path: Path):
+    image = imread(image_path.as_posix())
+
+    overlap = None
+
+    chunks = None
+    if args.chunks is not None:
+        chunks = args.chunks
+        overlap = 64
+    tiles = da.from_array(image, chunks=chunks)
+    return tiles, overlap
 
 
 def main():
@@ -49,10 +59,9 @@ def main():
     model_path = Path(args.model)
     model = load_model(model_path)
 
-    #Load image
+    #Load image info
     image_path = Path(args.image_path)
     image_name = image_path.stem
-    image = imread(image_path.as_posix())
 
 
     #File structuring
@@ -72,8 +81,19 @@ def main():
         raise e
     logging.info(f"Running cellpose with following kwargs: {kwargs}")
 
-    #Run predictions
+    #Run predictions on tiles image
     channels = [[0, 0]]
+    image_tiles, overlap = tile_image(image_path)
+
+    if overlap is not None:
+        tile_map = da.map_overlap(run_predictions,
+                                  image_tiles,
+                                  model=model,
+                                  channels=channels,
+                                  kwargs=kwargs,
+                                  depth=overlap)
+
+
     masks, flows, style = run_predictions(
         model,
         image,
