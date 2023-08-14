3b0d539d90590d740ad99d9fc7df31eeaa814ee3
Nate <natelharrison@gmail.com>
2023-08-14 14:44:53 -0700

    cellpose_run.py: idk

--- cellpose_run.py
+++ cellpose_run.py
@@ -27,7 +27,7 @@ parser.add_argument('--image_path', type=str, default='')
 parser.add_argument('--model', type=str, default=None)
 parser.add_argument('--chunks', type=int, nargs='+', default=None)
 parser.add_argument('--kwargs', type=str,
-    default='{"diameter": 0, "do_3D": true, "min_size": 5000, "augment": true}')
+    default='{"diameter": 30, "do_3D": true, "min_size": 5000, "augment": true}')
 parser.add_argument('--save_name', type=str, default=date_string)
 parser.add_argument('--batch_num', type=str, default=None)
 args = parser.parse_args()
@@ -44,7 +44,7 @@ def load_model(
 
 
 def run_predictions(model, image, channels, **kwargs):
-    mask, _, _ =  model.eval(image, channels=channels, batch_size=16, **kwargs)
+    mask, _, _ =  model.eval(image, channels=channels, batch_size=256, **kwargs)
 
     del image
     gc.collect()
