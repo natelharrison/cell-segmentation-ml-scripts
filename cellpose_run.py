57f6c0b6cec110d20be0b905d5f6d442b402391a
Nate <natelharrison@gmail.com>
2023-08-14 13:53:36 -0700

    cellpose_run.py: turned logging back on for run_predictions

--- cellpose_run.py
+++ cellpose_run.py
@@ -44,11 +44,7 @@ def load_model(
 
 
 def run_predictions(model, image, channels, **kwargs):
-    logging.getLogger('cellpose').setLevel(logging.WARNING)
-    tqdm.tqdm.disable = True
     mask, _, _ =  model.eval(image, channels=channels, batch_size=16, **kwargs)
-    tqdm.tqdm.disable = False
-    logging.getLogger('cellpose').setLevel(logging.INFO)
 
     del image
     gc.collect()
