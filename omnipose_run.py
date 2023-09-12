2be7c2d6b8038979b7aba240284e9c9a9a1f02c6
myrkgod <natelharrison@gmail.com>
2023-09-11 22:50:25 -0700

    omnipose_run.py: adjusting parameters for inference

--- omnipose_run.py
+++ omnipose_run.py
@@ -64,7 +64,7 @@ def main():
         verbose=True,
         tile=True,
         niter=150,
-        batch_size=12,
+        batch_size=8,
         flow_factor=10,
         mask_threshold=5,
         flow_threshold=-5,
