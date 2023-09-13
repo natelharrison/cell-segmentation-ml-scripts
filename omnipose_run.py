8ca4e82cedf6c487543b8cee73ec1d9c1a738650
myrkgod <natelharrison@gmail.com>
2023-09-13 13:40:56 -0700

    omnipose_run.py: added vram usage tracking, no clue if it actually works

--- omnipose_run.py
+++ omnipose_run.py
@@ -1,8 +1,11 @@
-import argparse
+import time
 import torch
+import argparse
 import omnipose
 import tifffile
+import threading
 import numpy as np
+import matplotlib.pyplot as plt
 
 from pathlib import Path
 from datetime import datetime
@@ -21,6 +24,32 @@ parser.add_argument('--kwargs', type=str, default=None)
 parser.add_argument('--save_name', type=str, default=date_string)
 parser.add_argument('--batch_num', type=str, default=None)
 args = parser.parse_args()
+
+gpu_memory_logs = []
+
+
+def monitor_gpu(interval=2):
+    num_gpus = torch.cuda.device_count()
+    while True:
+        mem_info = [torch.cuda.memory_allocated(device=i) / 1024 ** 2 for i in range(num_gpus)]
+        gpu_memory_logs.append(mem_info)
+        print(f"GPU Memory Used: {mem_info} MiB")
+        time.sleep(interval)
+
+
+def plot_memory_usage():
+    num_gpus = torch.cuda.device_count()
+    plt.figure(figsize=(10, 6))
+
+    for i in range(num_gpus):
+        plt.plot([log[i] for log in gpu_memory_logs], label=f'GPU {i}')
+
+    plt.title('GPU Memory Usage Over Time')
+    plt.xlabel('Time (every 2 seconds)')
+    plt.ylabel('Memory Used (MiB)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 
 def load_model(model_path: Path, **kwargs) -> models.CellposeModel:
@@ -83,4 +112,10 @@ def main():
 
 
 if __name__ == '__main__':
+    t = threading.Thread(target=monitor_gpu)
+    t.start()
+
     main()
+
+    t.join()
+    plot_memory_usage()
