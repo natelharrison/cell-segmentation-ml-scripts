10197f283588003611bcf1b99a00271edd88fae8
myrkgod <natelharrison@gmail.com>
2023-09-18 00:00:30 -0700

    clean_training_data.py: WORKING!!!!! Labels look nice, but still testing active contour parameters. Also need to speed up label processing by cropping inputs to roughly twice total label size.

--- clean_training_data.py
+++ clean_training_data.py
@@ -1,27 +1,48 @@
 import argparse
 from multiprocessing import Pool
+from typing import Tuple, Union
 
-import itk
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import SimpleITK as sitk
+
+from numpy import ndarray
 from pathlib import Path
 from tifffile import imread, imwrite
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--image_path', type=str)
-parser.add_argument('--mask_path', type=str)
-parser.add_argument('--remove_label', type=int)
+parser.add_argument('--image_path', type=str, help='Path to the input image')
+parser.add_argument('--mask_path', type=str, help='Path to the input mask')
+parser.add_argument('--remove_label', type=int, help='Label to be removed from the mask')
+parser.add_argument('--processes', type=int, help='Number of processes for parallel processing')
+parser.add_argument('--visualize', action='store_true', help='Flag to enable visualization')
 args = parser.parse_args()
 
 
-def get_label_slice(mask):
+def get_label_slice(mask: np.ndarray) -> ndarray[int]:
+    """
+    Get the index of the slice with the most prominent label in a 3D binary mask.
+
+    Parameters:
+    - mask (np.ndarray): The 3D binary mask.
+
+    Returns:
+    ndarray[int]: The index of the prominent slice.
+    """
     slice_sums = mask.sum(axis=(1, 2))
     return np.argmax(slice_sums)
 
 
 def visualize_3d_slice(image, mask, gradient_magnitude, refined_mask):
-    # Determine the prominent slice for the mask
+    """
+    Visualize slices of input image, mask, gradient magnitude, and refined mask.
+
+    Parameters:
+    - image: The input image.
+    - mask: The initial mask.
+    - gradient_magnitude: The gradient magnitude.
+    - refined_mask: The refined mask.
+    """
     slice_index = get_label_slice(mask)
 
     fig, axes = plt.subplots(1, 4, figsize=(20, 5))
@@ -50,40 +71,81 @@ def visualize_3d_slice(image, mask, gradient_magnitude, refined_mask):
     plt.show()
 
 
-def process_label(data: tuple[np.ndarray, np.ndarray, int]):
+def process_label(
+        data: Tuple[np.ndarray, np.ndarray, int]
+) -> Tuple[np.ndarray, int]:
+    """
+    Process a label within a Pool of processes.
+
+    Parameters:
+    - data (Tuple[np.ndarray, np.ndarray, int]): A tuple containing image, mask, and label.
+
+    Returns:
+    Tuple[np.ndarray, int]: A tuple containing the refined mask and label.
+    """
     image, mask, label = data
     print(f"Processing mask {label}")
     label_mask = (mask == label).astype(np.uint16)
     return active_countour(image, label_mask), label
 
 
-def active_countour(image: np.ndarray, binary_mask: np.ndarray) -> np.ndarray:
+def active_countour(
+        image: np.ndarray,
+        binary_mask: np.ndarray
+) -> np.ndarray[np.uint8]:
+    """
+    Perform active contour segmentation on an input image.
+
+    Parameters:
+    - image (np.ndarray): The input image.
+    - binary_mask (np.ndarray): Binary mask for active contour initialization.
+
+    Returns:
+    np.ndarray: The refined binary mask.
+    """
     itk_image = sitk.Cast(sitk.GetImageFromArray(image), sitk.sitkFloat32)
-    itk_mask = sitk.Cast(sitk.GetImageFromArray(binary_mask), sitk.sitkFloat32)
+    itk_binary_mask = sitk.GetImageFromArray(binary_mask.astype(np.uint8))
+
+    # Convert binary mask to a signed distance map
+    itk_mask = sitk.SignedMaurerDistanceMap(
+        itk_binary_mask,
+        insideIsPositive=True,
+        squaredDistance=False,
+        useImageSpacing=True
+    )
 
     gradient_magnitude = sitk.GradientMagnitudeRecursiveGaussian(
-        itk_image, sigma=1.0
+        itk_image, sigma=1  # Adjust the sigma value for Gaussian smoothing
     )
 
     # Geodesic active contour filter initialization
     img_filter = sitk.GeodesicActiveContourLevelSetImageFilter()
-    img_filter.SetPropagationScaling(1.0)
-    img_filter.SetCurvatureScaling(1.0)
-    img_filter.SetAdvectionScaling(1.0)
+    img_filter.SetPropagationScaling(-0.5)
+    img_filter.SetCurvatureScaling(10.0)
+    img_filter.SetAdvectionScaling(5.0)
     img_filter.SetMaximumRMSError(0.01)
-    img_filter.SetNumberOfIterations(1000)
+    img_filter.SetNumberOfIterations(250)
 
     refined_mask = img_filter.Execute(itk_mask, gradient_magnitude)
-    visualize_3d_slice(image, binary_mask, sitk.GetArrayFromImage(gradient_magnitude), sitk.GetArrayFromImage(refined_mask))
-    return sitk.GetArrayFromImage(refined_mask)
+
+    if args.visualize:
+        visualize_3d_slice(
+            sitk.GetArrayFromImage(itk_mask),
+            sitk.GetArrayFromImage(gradient_magnitude),
+            binary_mask,
+            sitk.GetArrayFromImage(refined_mask).astype(np.float16)
+        )
+
+    # Convert the refined mask into binary
+    return (sitk.GetArrayFromImage(refined_mask) > -2.0).astype(np.uint8)
 
 
 def main():
-    image_path = Path(args.image_path)
-    mask_path = Path(args.mask_path)
+    image_path: Union[str, Path] = Path(args.image_path)
+    mask_path: Union[str, Path] = Path(args.mask_path)
 
-    image: np.ndarray = imread(image_path.as_posix())
-    mask: np.ndarray = imread(mask_path.as_posix())
+    image: np.ndarray = imread(image_path)
+    mask: np.ndarray = imread(mask_path)
     labels, pixel_count = np.unique(mask, return_counts=True)
 
     if labels[0] == 1 and pixel_count[0] >= 50000:
@@ -91,14 +153,14 @@ def main():
         print(f"Removed label {labels[0]} of size {pixel_count[0]}")
 
     refined_mask = np.zeros_like(mask)
-    with Pool(processes=1) as pool:
+    with Pool(processes=args.processes) as pool:
         results = pool.map(
             process_label,
             [(image, mask, label) for label in labels[1:]]
         )
 
     for refined_label, label in results:
-        refined_mask[refined_label > 0.5] = label
+        refined_mask[refined_label == 1] = label
 
     save_path = mask_path.parent / "refined_mask.tif"
     imwrite(save_path, refined_mask)
