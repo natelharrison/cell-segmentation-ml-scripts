f3d9af85c606b8697ef62141b507086775a7792a
myrkgod <natelharrison@gmail.com>
2023-10-12 07:02:49 -0700

    cellpose_run.py: i'm tired...

--- omnipose_run.py
+++ omnipose_run.py
@@ -91,7 +91,7 @@ def main():
                 flow_threshold=-5
             )
 
-            iter_list = [50]
+            iter_list = [25, 50, 75, 100]
             for niter in iter_list:
                 mask, kwargs = run_mask_prediction(
                     flow,
