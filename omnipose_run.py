e7179c003370461b1c3b678a4b2e389d666bdc32
myrkgod <natelharrison@gmail.com>
2023-10-15 17:21:12 -0700

    omnipose_run.py: testing new clustering

--- omnipose_run.py
+++ omnipose_run.py
@@ -121,9 +121,9 @@ def main():
                     nclasses=2,
                     dim=3,
                     suppress=False,
-                    eps=25,
+                    eps=58,
                     hdbscan=False,
-                    min_samples=400,
+                    min_samples=4000,
                     flow_factor=5,  # not needed with suppression off
                     debug=False,
                     override=False)
