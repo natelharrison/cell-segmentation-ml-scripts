3a71c5afe6f8c68381585106ecd208053bf62ad5
natelharrison <natelharrison@gmail.com>
2024-04-12 09:49:34 -0700

    Hello

--- cellpose_run.py
+++ cellpose_run.py
@@ -47,7 +47,7 @@ def load_model(
 
 
 def run_predictions(model, image, channels, **kwargs):
-    mask, _, _, _ = model.eval(image, channels=channels, batch_size=256, **kwargs)
+    mask, _, _ = model.eval(image, channels=channels, batch_size=256, **kwargs)
 
     print("Predictions Done")
     return mask
